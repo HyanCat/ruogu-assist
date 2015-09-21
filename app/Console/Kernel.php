1d196b6eb11115117aff32fb849bf3e95d93d844
HyanCat <hyancat@live.cn>
2015-09-21 09:43:23 +0800

    定时任务变更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,13 +29,15 @@ class Kernel extends ConsoleKernel
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		// 每天 10 点开始，每个小时执行一次
-		if (Carbon::now()->hour >= 10) {
+		// 每天下午 1 点到 5 点，每个小时 100
+		if (Carbon::now()->hour >= 13 && Carbon::now()->hour <= 17) {
 			$schedule->command('mail:ad 100 --everytime=20')->hourly()->sendOutputTo(storage_path('schedule') . '/admail_' . Carbon::now()->format('Ymd_Hi') . '.log');
 		}
-		// 每天 22 点开始，清除无效邮件
-		if (Carbon::now()->hour >= 22) {
-			$schedule->command('mail:check --clear')->hourly()->sendOutputTo(storage_path('schedule') . '/checkmail_' . Carbon::now()->format('Ymd_Hi') . '.log');
+		// 每天 18 点发送剩下的
+		$schedule->command('mail:ad 800 --everytime=20')->dailyAt('18:00')->sendOutputTo(storage_path('schedule') . '/admail_' . Carbon::now()->format('Ymd_Hi') . '.log');
+		// 每天 23 点开始，清除无效邮件
+		if (Carbon::now()->hour >= 23) {
+			$schedule->command('mail:check --clear')->everyTenMinutes()->sendOutputTo(storage_path('schedule') . '/checkmail_' . Carbon::now()->format('Ymd_Hi') . '.log');
 		}
 	}
 }
