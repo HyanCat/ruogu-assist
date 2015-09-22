97411f800573b6b1996201a1d87e177e8381a1c0
HyanCat <hyancat@live.cn>
2015-09-22 10:50:18 +0800

    定时任务变更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,11 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-	/**
-	 * The Artisan commands provided by your application.
-	 *
-	 * @var array
-	 */
 	protected $commands = [
 		Commands\Inspire::class,
 		Commands\MailAd::class,
@@ -21,22 +16,19 @@ class Kernel extends ConsoleKernel
 		Commands\CrawQQUser::class,
 	];
 
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-	 * @return void
-	 */
 	protected function schedule(Schedule $schedule)
 	{
 		// 每天下午 1 点到 5 点，每个小时 100
 		if (Carbon::now()->hour >= 13 && Carbon::now()->hour <= 17) {
 			$schedule->command('mail:ad 100 --everytime=20')->hourly()->sendOutputTo(storage_path('schedule') . '/admail_' . Carbon::now()->format('Ymd_Hi') . '.log');
 		}
-		// 每天 18 点发送剩下的
+		// 每天 18 点发最多
 		$schedule->command('mail:ad 800 --everytime=20')->dailyAt('18:00')->sendOutputTo(storage_path('schedule') . '/admail_' . Carbon::now()->format('Ymd_Hi') . '.log');
+		// 每天 19 点发 300
+		$schedule->command('mail:ad 300 --everytime=20')->dailyAt('19:00')->sendOutputTo(storage_path('schedule') . '/admail_' . Carbon::now()->format('Ymd_Hi') . '.log');
+
 		// 每天 23 点开始，清除无效邮件
-		if (Carbon::now()->hour >= 23) {
+		if (Carbon::now()->hour == 23) {
 			$schedule->command('mail:check --clear')->everyTenMinutes()->sendOutputTo(storage_path('schedule') . '/checkmail_' . Carbon::now()->format('Ymd_Hi') . '.log');
 		}
 	}
